96d134d65f816d98e7a2820dd17ecd2ee87220df
Vishnu Kumar PS <vishnu.kumarps@claysys.net>
2023-04-13 21:36:48 +0530

    Add project files.

---
+++ Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace DevTestMate_MVC_App.Models;
+
+public class User
+{
+    public string Name { get; set; }
+
+    public string Email { get; set; }
+
+    public string Password { get; set; }
+}
